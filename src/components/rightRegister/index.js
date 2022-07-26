e9b6b938afda34158801693cf96c12a13ad60c19
Nguyễn Văn Tiến <96413838+NguyenVannTien@users.noreply.github.com>
2022-07-26 22:31:44 +0700

    update

--- src/components/rightRegister/index.js
+++ src/components/rightRegister/index.js
@@ -4,10 +4,24 @@ import Input from '../Input/Input'
 
 const RightRigister = () => {
   return (
-    <div className="container">
-      <form className='right-register'>
+    <div className="container-register">
+      <form className='form-register'>
         <h3>Personal Details</h3>
-        <Input  label="Firt Name"/>
+        <Input  label="Firt Name"  placeholder='Enter First Name' type='text' />
+        <Input  label="Last Name"  placeholder='Enter Last Name' type='text' />
+        <Input  label="Email Address"  placeholder='Enter Email Address' type='email' />
+          <>
+            <label>Phone Number</label>
+            <div className='form-register__phone'>
+              <select name="cars" id="cars">
+                <option value="vietnamese">+84</option>
+                <option value="USA">+213</option>
+                <option value="English">+63</option>
+                <option value="Lao">+1</option>
+              </select>
+              <input   placeholder='Enter Phone Number' type='text' /> 
+            </div> 
+          </>
       </form>
     </div>
   )
